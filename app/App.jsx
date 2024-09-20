8639a8b9d6fedece385285998032cf36c343e1a4
Tim Baines <timbaines@timbaines.com>
2024-09-20 00:59:56 -0400

    Step 5: Added code for imports, App components, state hook, event handlers, product list mods render method

--- app/App.jsx
+++ app/App.jsx
@@ -1,10 +1,48 @@
-function App() {
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { faker } from "@faker-js/faker"
+
+import Product from "./Product"
+import productsData from "./data"
+
+export default function App() {
+  const [count, setCount] = React.useState(0)
+
+  function increment() {
+    setCount(prevCount => prevCount + 1)
+  }
+
+  function decrement() {
+    setCount(prevCount => prevCount - 1)
+  }
+
+  const productsCopy = [...productsData]
+  productsCopy.push({
+    id: faker.database.mongodbObjectId(),
+    name: "+ Create new item"
+  })
 
   return (
-    <div>
+      <>
+        <h1>The current count is {count}</h1>
+        <button className="button" onClick={decrement}>-</button>
+        <button className="button" onClick={increment}>+</button>
+        <br />
+        <br />
+        <div className="products-list">
+          {
+            productsCopy.map(product => (
+                <Product key={product.id} product={product} />
+            ))
+          }
 
-    </div>
+        </div>
+      </>
   )
 }
 
-export default App
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+);
